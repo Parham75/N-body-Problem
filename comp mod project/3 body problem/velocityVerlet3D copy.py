73326fb52a07611dc585265ede9925d73ba78e19
Mohammadreza Aboutalebi <33571724+Parham75@users.noreply.github.com>
2019-01-29 15:01:48 +0000

    Add files via upload

---
+++ comp mod project/3 body problem/velocityVerlet3D copy.py
@@ -0,0 +1,168 @@
+"""
+By : Mohammadreza Aboutalebi
+UUN : s1664598
+date : Nov 2018
+
+CMod Ex3: velocity Verlet time integration of
+a molecule moving in a Morse potential.
+
+Produces plots of the position of the particle
+and its energy, both as function of time. Also
+saves both to file.
+
+potential D_e * ((1-exp[-a*(R - re)])**2 - 1)
+force -2aD_e(1-exp[-a*(R - re)])exp[-a*(R - re)]vecector_R/R
+D_e and a are parameters defined in the main() method
+and passed to the functions that
+calculate force and potential energy.
+
+"""
+
+import sys
+import math
+import numpy as np
+import matplotlib.pyplot as pyplot
+from Particle3D import Particle3D
+
+
+def force_dw(particle1, particle2):
+    """
+    Method to return the force on a particle in a Morse
+    potential given by: -2aD_e(1-exp[-a*(R - r_e)])exp[-a*(R - re)]vector_R/R
+    where R is separation between two atoms and vector_R is a vector from p1 to p2
+
+    :param particle1: Particle3D instance
+    :param particle2: Particle3D instance
+    :param D_e: parameter D_e from potential
+    :param r_e: parameter r_e from potential
+    :param alpha: parameter alpha from potential
+    :return: force acting on particle as a Numpy array
+    """
+
+    vector_R = Particle3D.Vector_Separation(particle1,particle2)
+    R = np.linalg.norm(vector_R)
+    force = -2*alpha*D_e*(1-math.exp(-alpha*(R - r_e)))*math.exp(-alpha*(R - r_e))/R
+    return force*vector_R
+
+
+
+def make_pyplot(x, y, label_y):
+    """
+    Method to make plots of two lists of numbers
+    where the first is the time.
+
+    :param x: x values of the plot
+    :param y: y values of the plot
+    :param label_y: label of the axis y
+    """
+
+    pyplot.title('velocityVerlet: '+ label_y+' against time')
+    pyplot.xlabel('time')
+    pyplot.ylabel(label_y)
+    pyplot.plot(x, y)
+    pyplot.show()
+
+# Begin main code
+def main(argv1, argv2, argv3):
+
+    # Read name of output file from command line
+    if len(sys.argv)!=4:
+        print("Wrong number of arguments.")
+        print("Usage: " + sys.argv[0] + " <output file> <molecule name> <True or False for spin>")
+        quit()
+    else:
+        outfile_name = sys.argv[1]
+        molecule = sys.argv[2]
+        spin = sys.argv[3]
+
+    # knoowing input initialise filename based on files in directory
+    if spin == "True":
+        input_file_name = molecule + "withspin.txt"
+        print("When we start, the molecule has a spin in the y direction.")
+    else:
+        input_file_name = molecule + ".txt"
+        print("When we start, the molecule has no spin.")
+        
+    # Open input and output file
+    outfile = open(outfile_name, "w")
+    file_parameters = open("parameters.txt", "r")
+    input_file = open(input_file_name, "r")
+
+    # Set up simulation parameters
+    dt = 0.1
+    numstep = 200
+    time = 0.0
+
+    # Set up two particles initial conditions and energy from input_file:
+
+    p1 = Particle3D.from_file(input_file)
+    p2 = Particle3D.from_file(input_file)
+    p3 = Particle3D.from_file(input_file)
+
+    outfile.write("{0:f} {1:f} {2:12f}\n".format(time,pos_list))
+
+    # Get initial force
+    force_1 = force_dw(p1, p2) + force_dw(p1, p3)
+    force_2 = force_dw(p2, p3) + force_dw(p2, p1)
+    force_3 = force_dw(p3, p1) + force_dw(p3, p2)
+
+    # Initialise data lists for plotting later
+    time_list = [time]
+    pos_list = [p1.position, p2.position, p3.position]
+
+    # Start the time integration loop
+
+    for i in range(numstep):
+        # Update particle position
+        p1.position = p1.leap_pos2nd(dt, force_1)
+        p2.position = p2.leap_pos2nd(dt, force_2)
+        p3.position = p3.leap_pos2nd(dt, force_3)
+
+        # Update force
+        force_new_1 = force_dw(p1, p2) + force_dw(p1, p3)
+        force_new_2 = force_dw(p2, p3) + force_dw(p2, p1)
+        force_new_3 = force_dw(p3, p1) + force_dw(p3, p2)
+
+        # Update particle velocity by averaging
+        # current and new forces
+        p1.leap_velocity(dt, 0.5*(force_1+force_new_1))
+        p2.leap_velocity(dt, 0.5*(force_2+force_new_2))
+        p3.leap_velocity(dt, 0.5*(force_3+force_new_3))
+
+        # Re-define force value
+        force_1 = force_new_1
+        force_2 = force_new_2
+        force_3 = force_new_3
+
+
+        # Increase time
+        time = time + dt
+        
+
+        # Append information to data lists
+        time_list.append(time)
+        pos_list.append(p1.position, p2.position, p3.position)
+
+        # write in the output file
+        outfile.write("{0:f} {1:f} {2:12f}\n".format(time,pos_list))
+
+    # Post-simulation:
+    
+    # Close all files
+    outfile.close()
+    input_file.close()
+    file_parameters.close()
+
+   # Make vector separation and energy plots
+    make_pyplot(time_list, pos_list, "vector separation")
+    make_pyplot(time_list, energy_list, "Potential Energy")
+
+    # fluctuation for energy
+    mean_en = energy_list[0]
+    max_en = np.max(energy_list)
+    print("The energy fluctuation is {0:6f}".format(math.abs((max_en-mean_en)/mean_en)))
+
+
+# Execute main method:
+if __name__ == "__main__":
+    main(sys.argv[1],sys.argv[2],sys.argv[3])
